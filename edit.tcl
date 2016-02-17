480fb7fe4bf8cacb1425b35c723628e16efe8c59
Danyil Bohdan <danyil.bohdan@gmail.com>
2016-02-17 20:11:22 +0200

    Reformat edit.tcl

--- edit.tcl
+++ edit.tcl
@@ -20,9 +20,11 @@ proc edit encrypted {
 
     set temporary [::fileutil::tempfile]
     file attributes $temporary -permissions 0600
-    exec {*}$commandPrefix --decrypt -o $temporary $encrypted << $passphrase
+    exec {*}$commandPrefix --decrypt -o $temporary $encrypted \
+            << $passphrase
     exec $::env(EDITOR) $temporary <@ stdin >@ stdout 2>@ stderr
-    exec {*}$commandPrefix --symmetric --armor -o $encrypted $temporary << $passphrase
+    exec {*}$commandPrefix --symmetric --armor -o $encrypted $temporary \
+            << $passphrase
     file delete $temporary
 }
 
