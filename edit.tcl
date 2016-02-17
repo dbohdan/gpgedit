6778eab277d2be64b202c9e401c0e93c0f185494
Danyil Bohdan <danyil.bohdan@gmail.com>
2016-02-17 20:09:57 +0200

    edit.tcl: Use `stty` on *nix

--- edit.tcl
+++ edit.tcl
@@ -2,10 +2,22 @@
 package require Tcl 8.5
 package require fileutil
 
+set GPG2_PATH gpg2
+
 proc edit encrypted {
-    set commandPrefix {-ignorestderr -- gpg2 --batch --yes --passphrase-fd 0}
+    set commandPrefix [list -ignorestderr -- \
+            $::GPG2_PATH --batch --yes --passphrase-fd 0]
     puts Passphrase:
+
+    if {$::tcl_platform(platform) eq {unix}} {
+        set oldMode [exec stty -g <@ stdin]
+        exec stty -echo <@ stdin
+    }
     gets stdin passphrase
+    if {$::tcl_platform(platform) eq {unix}} {
+        exec stty {*}$oldMode <@ stdin
+    }
+
     set temporary [::fileutil::tempfile]
     file attributes $temporary -permissions 0600
     exec {*}$commandPrefix --decrypt -o $temporary $encrypted << $passphrase
