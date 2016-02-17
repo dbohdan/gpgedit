0debdf0316dc400778367e6da32240ccf825742e
Danyil Bohdan <danyil.bohdan@gmail.com>
2016-02-17 20:18:49 +0200

    Add command line option "-editor"

--- edit.tcl
+++ edit.tcl
@@ -1,10 +1,11 @@
 #!/usr/bin/env tclsh
 package require Tcl 8.5
+package require cmdline
 package require fileutil
 
 set GPG2_PATH gpg2
 
-proc edit encrypted {
+proc edit {encrypted editor} {
     set commandPrefix [list -ignorestderr -- \
             $::GPG2_PATH --batch --yes --passphrase-fd 0]
     puts Passphrase:
@@ -22,10 +23,24 @@ proc edit encrypted {
     file attributes $temporary -permissions 0600
     exec {*}$commandPrefix --decrypt -o $temporary $encrypted \
             << $passphrase
-    exec $::env(EDITOR) $temporary <@ stdin >@ stdout 2>@ stderr
+    exec $editor $temporary <@ stdin >@ stdout 2>@ stderr
     exec {*}$commandPrefix --symmetric --armor -o $encrypted $temporary \
             << $passphrase
     file delete $temporary
 }
 
-edit [lindex $argv 0]
+proc main {argv0 argv} {
+    set options {
+        {editor.arg  {}  {editor to use}}
+    }
+    set usage "$argv0 \[options] filename ...\noptions:"
+    set opts [::cmdline::getoptions argv $options $usage]
+
+    set editor $::env(EDITOR)
+    if {[dict exists $opts editor]} {
+        set editor [dict get $opts editor]
+    }
+    edit [lindex $argv 0] $editor
+}
+
+main $argv0 $argv
